c60a1f2d6fd9b6da8c1fb97cca7b477f56b1e073
Black Cranium <mikhail.zabolotskih@gmail.com>
2022-06-23 20:59:37 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,11 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+wrds = text.split()
+i = 0
+count = 0
+while i < len(wrds):
+    if len(wrds[i]) > 5:
+        count += 1
+    i += 1
+print(count)
